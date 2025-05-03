5c42cefeb62b325768196dde22aa68eba88f3a9c
INSTRUCTORBRAVO <jbravos@certus.edu.pe>
2025-05-02 22:02:46 -0500

    VERSION 02-05 JS

--- SCRIPT1.js
+++ SCRIPT1.js
@@ -33,14 +33,34 @@ function FCambioTag1() {
     vTexto3[13].innerHTML="Elemento ultimo h2 cambiado por TagDom1";
     vTexto3[13].style.color="magenta";
     //indice 10 es el 11vo elemento h2
+    console.log("Tamaño del array=",vTexto3.length);
+    vTexto3[13].insertAdjacentHTML("beforeEnd", "<br>tamaño de h2 =" + vTexto3.length);
+    let totalh2=vTexto3.length;
+    console.log("Valor de totalh2=",totalh2);
+    console.log("tipo de variable totalh2=", typeof totalh2);
+}
+
+ window.vTexto1="";
+
+function FAgregarTextoPermanente()
+{
+  let textoAdicional ="\nTexto adicional para concatenar";
+  vTexto1 = vTexto1 + textoAdicional;
+  console.log("tipo de variable de vTexto1 =",typeof  vTexto1);
+  console.log(vTexto1);
 }
 
 
 
+function FcrearH1()
+{
+var elemento =document.getElementById("idCrearH1");
+var nuevoH1 =document.createElement("H1");
+var texto = document.createTextNode("Texto colocado en linea");
+nuevoH1.appendChild(texto);
+elemento.appendChild(nuevoH1);
 
-
-
-
+}
 
 
 
@@ -68,16 +88,18 @@ function FCapturarNombre(){
 
     const vTexto1 = document.getElementById("nombre");
     const vTexto2 = document.querySelectorAll("input.datosPersonales");
-   
+    const vTexto3 = document.getElementById("clave");
+
+   // usar input para clases relacioandas a cajas de texto, etc
     vTexto1.style.color="red";
     
-    var valor1 =vTexto1.value;
+    var valor1 =vTexto1.value; // value obtiene el contenido de la caja de texto nombre
     var valor2 ="";
-
-    document.getElementById('observacion').value = valor1;
-   
+    vTexto3.value=valor1;
+    document.getElementById("observacion").value = valor1;//textarea ya captura el nombre
+   //foreach recorre todo el contenido del array de la clase datosPersonales
     vTexto2.forEach(input => {
-        valor2 =valor2 + `${input.name}: ${input.value}\n`;
+        valor2 =valor2 +  `${input.name}: ${input.value}\n`; // alt+96
       });
 
     document.getElementById('observacion').value = valor2;
