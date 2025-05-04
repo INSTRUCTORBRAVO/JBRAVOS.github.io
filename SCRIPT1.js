7a54d80ef190506cd63a76b13a4fd2642b88f4bb
INSTRUCTORBRAVO <jbravos@certus.edu.pe>
2025-05-03 19:17:38 -0500

    VERSION sabado 03-05 JS

--- SCRIPT1.js
+++ SCRIPT1.js
@@ -106,3 +106,40 @@ function FCapturarNombre(){
     console.log('El valor del nombre es:', valor1);
 }
 
+
+function FUsoFor1()
+{
+  const vTextoClass = document.getElementsByClassName("classFOR1");
+  var vTextoTotal="";
+  // como length=4 --> ira desde 0 hasta 3
+  for(let i=0 ; i< vTextoClass.length ; i++ )
+  {
+    vTextoClass[i].style.border ="2px solid red" ;
+    vTextoTotal=vTextoTotal + vTextoClass[i].innerText+'-';
+
+  } // fin de for
+  var nuevoH1 =document.createElement("h1");
+  var texto = document.createTextNode(vTextoTotal);
+  nuevoH1.appendChild(texto);
+  vTextoClass[3].appendChild(nuevoH1);
+  
+}  // fin de function
+
+function FEliminarConcat()
+{
+  const vTextoClass = document.getElementsByClassName("classFOR1");
+  let rpta =prompt("Estas seguro de eliminar el texto concatenado?(S/N)",'');
+  if (rpta=="S" || rpta=="s")  //  doble || significa o
+    { vTextoClass[3].remove();  // eliminar la class de posicion [3]
+      alert("CONCATENCION ELMINADA");
+    }  
+  else
+    { if(rpta=="N" || rpta=="n")
+      { alert("NO SE elimnará");
+      }
+      else
+      { alert("OPCION INCORRECTA-INgresa nuevamente");
+      }
+    }
+  //removeChild funciona cuando un nodo(class, id, etx) esta dentro de otro
+}
